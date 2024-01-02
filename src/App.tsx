8ee2c33b8f2b384538808b5b6906c1284da03c82
Yarin134 <yarindavid24@gmail.com>
2024-01-02 22:43:37 +0200

    header => not finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,21 @@ import Button from "./components/Atoms/Button/Button";
 
 function App() {
   return (
-    <>
-      <Button title="Leave" />
-    </>
+    <div
+      style={{
+        top: 0,
+        display: "flex",
+        paddingTop: "20px",
+        justifyContent: "center",
+      }}
+    >
+      <div className="header">
+        <div className="header-leave">
+          <Button title="LEAVE" />
+        </div>
+        <div className="header-title">My Room</div>
+      </div>
+    </div>
   );
 }
 
